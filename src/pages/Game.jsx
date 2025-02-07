4e03732da9f4a7679568a2982da24ee083383481
Ralf Siewert <ralf@actyvy.st>
2025-02-07 14:54:52 +0100

    enable selection of letters in alphabet component

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,35 @@
 import { Link } from "react-router";
 import DisplayWord from "../components/DisplayWord";
 import AlphabetDisplay from "../components/AlphabetDisplay";
-import { alphabet } from "../util/alphabet";
+import { alphabet as initialAlphabet } from "../util/alphabet";
+import { useState } from "react";
 import "./Game.css";
 
 export default function Game() {
+  const [fullWord, setFullWord] = useState("FRIDAY");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
   function handleLetterSelect(letter) {
+    if (!guessedLetters.includes(letter)) {
+      setGuessedLetters([...guessedLetters, letter]);
+    }
     console.log("handleLetterSelect called with letter:", letter);
   }
+
+  const alphabet = initialAlphabet.map((item) => {
+    if (guessedLetters.includes(item.letter)) {
+      return {
+        letter: item.letter,
+        isSelected: true,
+      };
+    } else {
+      return {
+        letter: item.letter,
+        isSelected: false,
+      };
+    }
+  });
+
   return (
     <section className="game_section">
       <DisplayWord></DisplayWord>
@@ -18,6 +40,8 @@ export default function Game() {
       <Link to="/result" className="result_btn">
         See results
       </Link>
+      <p>{JSON.stringify(guessedLetters)}</p>
+      <p>{JSON.stringify(alphabet)}</p>
     </section>
   );
 }
