bebf509ac79fe251b377ca14469ebc2c762c2d65
Stacy Volvis <strelka1515@gmail.com>
2025-02-07 12:54:05 +0100

    add displayWord component, change styles

---
+++ src/components/DisplayWord.jsx
@@ -0,0 +1,29 @@
+import "./DisplayWord.css";
+
+function DisplayWord({ word = "Di??icul??" }) {
+  let result = [];
+
+  if (typeof word === "string") {
+    for (let letter of word) {
+      result.push(letter);
+    }
+  } else {
+    result = word;
+  }
+
+  console.log(result);
+
+  return (
+    <div className="word_container">
+      <h3>Guess the word!</h3>
+
+      <div className="word_item">
+        {result.map((item) => {
+          return item === "?" ? <span>_</span> : <span>{item}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default DisplayWord;
